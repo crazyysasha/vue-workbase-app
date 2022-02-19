e88b64499068082d9fae58397869f5f1b2950a2b
ArtemioLangepas <39931236+ArtemioLangepas@users.noreply.github.com>
2022-02-19 16:47:10 +0500

    fix header

--- src/components/icons/hero/index.js
+++ src/components/icons/hero/index.js
@@ -2,3 +2,4 @@ export * as HLocation from './h-location';
 export * as HEnter from './h-enter';
 export * as HPencil from './h-pencil';
 export * as HX from './h-x';
+export * as HX from './h-order';
