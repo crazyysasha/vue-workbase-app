2e36413f93b4911ac504bb8f475f41303547e067
Doniyorov <85107650+Doniyorov@users.noreply.github.com>
2022-03-04 10:53:38 +0500

    fix mix

---
+++ src/components/wallet/c-select.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="relative">
+    <div
+      class="flex justify-between items-center border p-2 mt-3 cursor-pointer"
+      @click="isOpen = !isOpen"
+    >
+      <h5 v-text="text"></h5>
+      <svg
+        v-show="isOpen"
+        class="w-4"
+        viewBox="0 0 330 330"
+        style="enable-background: new 0 0 330 330"
+        xml:space="preserve"
+      >
+        <path
+          id="XMLID_224_"
+          d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394
+                l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393
+                C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"
+        />
+      </svg>
+      <svg
+        v-show="!isOpen"
+        class="w-4"
+        viewBox="0 0 330 330"
+        style="enable-background: new 0 0 330 330"
+        xml:space="preserve"
+      >
+        <path
+          id="XMLID_225_"
+          d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+        />
+      </svg>
+    </div>
+
+    <div
+      v-show="isOpen"
+      class="absolute bg-primary text-white top-12 border py-2 px-4"
+    >
+      <ul>
+        <li
+          v-for="item in items"
+          @click="text = item"
+          :key="item"
+          class="cursor-pointer rounded-md px-2 py-1 hover:bg-blue-500"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+const { ref } = require("@vue/reactivity");
+
+const isOpen = ref(false);
+const text = ref("Select Month");
+const items = ref([
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]);
+</script>
---
+++ src/views/WalletView.vue
@@ -0,0 +1,150 @@
+<template>
+  <section class="container">
+    <div class="grid grid-cols-4 gap-4 mt-8 px-10 pb-96">
+      <div class="col-span-2">
+        <div class="border border-primary p-6">
+          <div>
+            <h2 class="text-xl font-medium my-3">Select Payment Method</h2>
+          </div>
+          <div class="grid grid-cols-3 gap-4">
+            <div class="flex items-center border border-primary p-3">
+              <div class="w-14">
+                <img
+                  src="../assets/images/visa.png"
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <h5 class="text-lg font-semibold">Visa card</h5>
+            </div>
+
+            <div class="flex items-center border border-primary p-3">
+              <div class="w-14">
+                <img
+                  src="../assets/images/visa.png"
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <h5 class="text-lg font-semibold">Visa card</h5>
+            </div>
+
+            <div class="flex items-center border border-primary p-3">
+              <div class="w-14">
+                <img
+                  src="../assets/images/visa.png"
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <h5 class="text-lg font-semibold">Visa card</h5>
+            </div>
+          </div>
+
+          <div class="my-4">
+            <label for="NameCard"> Name On Card </label>
+            <input
+              class="
+                w-full
+                h-10
+                px-4
+                shadow
+                mt-3
+                border
+                focus:outline-none
+                bg-transparent
+                focus:shadow-lg focus:border-primary
+              "
+              type="text"
+              id="NameCard"
+            />
+          </div>
+
+          <div class="my-4">
+            <label class="block" for="CardNumber"> Card Number </label>
+            <div class="flex shadow border mt-3" :class="{'border-primary shadow': isfocus}">
+              <input
+                type="text"
+                maxlength="19"
+                pattern="[0-9]*"
+                inputmode="numeric"
+                @focus="isfocus = true"
+                v-model="creditCardNumber"
+                class="w-5/12 h-10 px-4 focus:outline-none bg-transparent"
+                id="CardNumber"
+                placeholder="0000 - 0000 - 0000 - 0000"
+              />
+              <div class="w-7/12 h-10 bg-transparent"></div>
+            </div>
+          </div>
+
+          <div class="my-4">
+            <div class="grid grid-cols-3 gap-4">
+              <div class="">
+                <h5>Month</h5>
+                <c-select></c-select>
+              </div>
+
+              <div class="">
+                <h5>Year</h5>
+                <div class="border p-2 mt-3">
+                  <h5>Select Year</h5>
+                </div>
+              </div>
+
+              <div class="">
+                <h5>CVV</h5>
+                <div class="border p-2 mt-3">SVV</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-span-1">
+        <div class="border border-primary p-6 h-80"></div>
+
+        <button
+          class="
+            border-2 border-primary
+            w-full
+            mt-4
+            py-3
+            text-center text-xl
+            font-medium
+            text-primary
+            transition-all
+            duration-500
+            hover:text-white
+            hover:bg-primary
+            hover:shadow-xl
+            hover:shadow-blue-400
+          "
+        >
+          PURCHASE NOW
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import CSelect from "@/components/wallet/c-select.vue";
+import { ref, watch } from "vue";
+const isfocus = ref(false);
+const creditCardNumber = ref("");
+
+watch(creditCardNumber, () => {
+  let realNumber = creditCardNumber.value.replace(/-/gi, "");
+
+  // Generate dashed number
+  let dashedNumber = realNumber.match(/.{1,4}/g);
+  creditCardNumber.value = dashedNumber.join("-");
+});
+</script>
+
+<style>
+</style>
