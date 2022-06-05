016dc2b060fb8e21d5965ad403bff8ef00048b21
ArtemioLangepas <39931236+ArtemioLangepas@users.noreply.github.com>
2022-06-05 19:56:51 +0500

    fix

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -320,6 +320,7 @@
 					</button>
 				</div>
 			</div>
+			
 			<div class="md:col-span-5">
 				<div
 					class="
@@ -715,6 +716,261 @@
 								</div>
 							</template>
 						</list-item>
+					</div>
+				</div>
+			</div>
+			<div class="md:col-span-5">
+				<div
+					class="
+						rounded-lg
+						bg-white
+						shadow shadow-primary/25
+						p-7
+						mb-6
+					"
+				>
+					<div class="w-full">
+						<div
+							class="
+								flex
+								items-center
+								border-b border-gray-300
+								pl-3
+								py-3
+							"
+						>
+							<img
+								class="h-16 w-16 rounded-full object-cover"
+								src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+								alt="username"
+							/>
+							<div class="">
+								<span class="block ml-2 font-bold text-lg"
+									>Мубаракшин Амаль</span
+								>
+								<div class="text-xs ml-2 flex items-center">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-3 w-3 mr-1"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										stroke-width="2"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+										/>
+									</svg>
+									<span>4.7 - 7 отзывов</span>
+								</div>
+								<div
+									class="
+										text-green-600
+										ml-2
+										leading-none
+										mb-2
+									"
+								>
+									<span
+										class="
+											w-2.5
+											h-2.5
+											rounded-full
+											bg-green-600
+											inline-block
+										"
+									></span>
+									Онлайн
+								</div>
+							</div>
+						</div>
+						<div
+							class="w-full min-h-[400px] overflow-y-auto py-10 relative"
+						>
+							<ul>
+								<li class="clearfix2">
+									<div class="w-full flex justify-start">
+										<div
+											class="
+												bg-gray-100
+												rounded
+												px-5
+												py-2
+												my-2
+												relative
+												max-w-[300px]
+											"
+										>
+											<span class="block">Привет!</span>
+											<span
+												class="
+													block
+													text-gray-500
+													text-xs
+													text-right
+												"
+												>10:30</span
+											>
+										</div>
+									</div>
+									<div class="w-full flex justify-end">
+										<div
+											class="
+												bg-gray-100
+												rounded
+												px-5
+												py-2
+												my-2
+												relative
+												max-w-[300px]
+											"
+										>
+											<span class="block"
+												>Добрый день</span
+											>
+											<span
+												class="
+													block
+													text-gray-500
+													text-xs
+													text-left
+												"
+												>10:32</span
+											>
+										</div>
+									</div>
+									<div class="w-full flex justify-end">
+										<div
+											class="
+												bg-gray-100
+												rounded
+												px-5
+												py-2
+												my-2
+												relative
+												max-w-[300px]
+											"
+										>
+											<span class="block"
+												>Как ваши дела?</span
+											>
+											<span
+												class="
+													block
+													text-gray-500
+													text-xs
+													text-left
+												"
+												>10:32</span
+											>
+										</div>
+									</div>
+									<div class="w-full flex justify-start">
+										<div
+											class="
+												bg-gray-100
+												rounded
+												px-5
+												py-2
+												my-2
+												relative
+												max-w-[300px]
+											"
+										>
+											<span class="block">Круто)</span>
+											<span
+												class="
+													block
+													text-gray-500
+													text-xs
+													text-right
+												"
+												>10:42</span
+											>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+
+						<div
+							class="
+								w-full
+								py-3
+								px-3
+								flex
+								items-center
+								justify-between
+								border-t border-gray-300
+								
+							"
+						>
+							<button class="outline-none focus:outline-none">
+								<svg
+									class="text-gray-400 h-6 w-6 hover:text-primary"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+									/>
+								</svg>
+							</button>
+
+							<input
+								
+								placeholder="Сообщение"
+								class="
+									py-2
+									mx-3
+									pl-5
+									block
+									w-full
+									rounded-full
+									bg-gray-100
+									outline-none
+									border
+									border-gray-100
+									focus:border
+									focus:border-primary
+								"
+								type="text"
+								name="message"
+								required
+							/>
+
+							<button
+								class
+								text-gray-500="outline-none focus:outline-none"
+								type="submit"
+							>
+								<svg
+									class="
+										text-gray-400
+										h-7
+										w-7
+										origin-center
+										transform
+										rotate-90
+										hover:text-primary
+									"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+								>
+									<path
+										d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
+									/>
+								</svg>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
